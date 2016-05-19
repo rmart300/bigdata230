c3828a112d34a6d5320015518a69d57e6ae7c62b
Cloud User <ec2-user@ip-172-31-2-201.us-west-2.compute.internal>
2016-05-19 01:30:11 -0400

    updated to exit gracefully

--- interactWithHive.sh
+++ interactWithHive.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/bash
 #https://www.mapr.com/blog/quick-tips-using-hive-shell-inside-scripts
 
-accession="K123456"
-queryFile="queryFile.fas"
-subjectFile="reference_sequence_subtype.fas"
+accession=$0
+queryFile=$1
+subjectFile=$2
 
 sampleMut=$(hive -S -e -hiveconf accession=$accession "use default; select count(*) from sample_mutation where accession = ${hiveconf:$accession});
 
-if $sampleMut > 0 then
-	exit(0)
+if $sampleMut = 0 then
+	`perl consumer.pl $accession $queryFile $subjectFile`
+
+	`pig loadMutationsIntoHive.pig`
+else
+	echo "$accession already in database"
 fi
-
-`perl consumer.pl $accession $queryFile $subjectFile`
-
-`pig loadMutationsIntoHive.pig`
-
