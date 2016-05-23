5e0eecfb8f6f3cc415039d2b82a5a564920b8c7d
Cloud User <ec2-user@ip-172-31-2-201.us-west-2.compute.internal>
2016-05-23 01:22:10 -0400

    updated interactive hive statement

--- interactWithHive.sh
+++ interactWithHive.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 #https://www.mapr.com/blog/quick-tips-using-hive-shell-inside-scripts
 
-accession=$0
-queryFile=$1
-subjectFile=$2
-referenceFile=$3
+accession=$1
+queryFile=$2
+subjectFile=$3
+referenceFile=$4
 
-sampleMut=$(hive -S -e -hiveconf accession=$accession "use default; select count(*) from sample_mutation where accession = ${hiveconf:$accession});
+echo "$accession"
+
+sampleMut=$(hive -S -hiveconf accession=$accession -e "select count(*) from sample_mutation where accession='${hiveconf:accession}';")
 
 if $sampleMut = 0 then
 	`perl consumer.pl $accession $queryFile $subjectFile $referenceFile`
